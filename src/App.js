73c2e778df592979b960b9e94b101b249600029e
Ayush Pansari <ayushgayush120@gmail.com>
2021-11-26 20:00:26 +0530

    local storage connected

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import './App.css';
 import Task from './components/Task'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+
+  const getLocalStorage = ()=>{
+    return JSON.parse(localStorage.getItem('textt'));
+  }
+  
+  const [tasks, setTasks] = useState(getLocalStorage());
   const [text, setText] = useState('');
 
-  const onChangeTask = (e) =>{
+  useEffect( () => {
+    localStorage.setItem('textt', JSON.stringify(tasks))
+  }, [tasks])
+
+  const onChangeTask = (e) => {
     setText(e.target.value);
   }
 
-  const submitTask = () =>{
+  const submitTask = () => {
     setTasks([...tasks, text]);
     setText('')
   }
 
-  const isDisabled = (text) =>{
-    if(text==='') return true;
+  const isDisabled = (text) => {
+    if (text === '') return true;
     return false;
   }
 
   return (
-    <div className="container">
-      <h1 id="main-heading">Task Manager</h1>
-      <form>
-      <div className="add-task">
-        <input type="text" placeholder="Task to add" value={text} onChange={onChangeTask} />
-        <button className="btn" onClick={submitTask} disabled={isDisabled(text)} >Add</button>
+    <>
+      <div className="container">
+        <h1 id="main-heading">Task Manager</h1>
+        <form>
+          <div className="add-task">
+            <input type="text" placeholder="Task to add" value={text} onChange={onChangeTask} />
+            <button className="btn" onClick={submitTask} disabled={isDisabled(text)} >Add</button>
+          </div>
+        </form>
+        <div id="display-task">
+          {tasks.map((task, index) => {
+            return <Task num={index + 1} text={task} tasks={tasks} setTasks={setTasks} />
+          })}
+        </div>
       </div>
-      </form>
-      <div id="display-task">
-        {tasks.map((task, index)=>{
-          return <Task num={index+1} text={task} tasks={tasks} setTasks={setTasks}/>
-        })}
-      </div>
-    </div>
+    </>
   );
 }
 
